bf27ffe22d65274b0a198598c5d203f65ec2109e
Erdi Çakmak <90332095+erdicakmak05@users.noreply.github.com>
2022-03-29 01:27:54 +0300

    first commit

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,22 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+                features = "src/test/resources/features",
+                glue = "stepdefinitions",
+                tags = "@smoke",
+                plugin = {
+                        "progress",
+                        "json:json-reports/cucumber.json",
+                        "html:build/cucumber-report/cucumber.html",
+                        "junit:build/cucumber-report/cucumber.xml"}
+        )
+        // plugin = { "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
+public class TestRunner {
+
+}
